987859fcd794a626501caf1fb07ef3dc291e9aa6
ser.poh <belikd484@gmail.com>
2026-05-04 13:12:48 +0200

    pass uid in url

--- handlers/start.py
+++ handlers/start.py
@@ -1,15 +1,27 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 from database.db import register_user, get_balance
-from keyboards.menus import main_menu
 
 router = Router()
 
+
 @router.message(CommandStart())
 async def start(message: Message):
-    await register_user(message.from_user.id, message.from_user.username)
-    balance = await get_balance(message.from_user.id)
+    user_id = message.from_user.id
+    await register_user(user_id, message.from_user.username)
+    balance = await get_balance(user_id)
+
+    url = f"https://ser-poh.github.io/casino-bot?uid={user_id}"
+
+    main_menu = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="🎰 Открыть Cristal Case", web_app=WebAppInfo(url=url))]
+        ],
+        resize_keyboard=True
+    )
+
     await message.answer(
         f"👋 Привет, {message.from_user.first_name}!\n\n"
         f"🎰 Добро пожаловать в <b>Cristal Case</b>!\n\n"
